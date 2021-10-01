10ddf0dcca6a3ce6ab7c544c4d05f511a33d0277
pavelFaraday <devstudio.corp@gmail.com>
2021-10-01 13:26:51 +0400

    APIs - Rest API | axios.post('')

--- Rest API/react-rest/src/App.js
+++ Rest API/react-rest/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Postlist from "./API/Postlist";
+import PostForm from "./API/PostForm";
 
 function App() {
   return (
     <div className="App">
+      <PostForm></PostForm>
       <Postlist></Postlist>
     </div>
   );
